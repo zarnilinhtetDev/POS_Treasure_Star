8db269541f26e4f250a48c7124f6396d85fe9cb1
zarnilinhtetDev <zarnilinhtet.mm@gmail.com>
2024-07-02 15:48:19 +0700

    add logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>SSE POS</title>
+    <title>SSE Point of Sale</title>
 
     <link rel="shortcut icon" href="{{ asset('img/logo.jpg') }}" type="image/x-icon">
     <!-- Fonts -->
